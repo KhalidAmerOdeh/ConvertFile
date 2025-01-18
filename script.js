86cc63ee2b12239a463e86a08e4b8c9cc84e5d93
Khalid Amer Odeh <Khalid.Amer.Adnan.Odeh123@gmail.com>
2025-01-18 21:22:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,16 @@ function compressFile() {
     alert("تم الضغط بنجاح!");
 }
 
+// تنزيل الملف تلقائيًا
+function downloadFile(fileUrl, fileName) {
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
 // التمرير السلس إلى أعلى الصفحة
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
